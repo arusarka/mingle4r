a1de60c59c29bddd6848115f4918e18c84d81a24
arusarh <arusarh@arusarhs-macbook-pro.local>
2009-07-20 13:06:27 +0530

    changed spec

--- spec/mingle4r_spec.rb
+++ spec/mingle4r_spec.rb
@@ -1,11 +1,11 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe Mingle4r do
-  it "should return my name" do
+  xit "should return my name" do
     Mingle4r::AUTHOR.should == 'Asur'
   end
   
   it "should return proper version" do
-    Mingle4r::VERSION.should == '0.0.1'
+    Mingle4r::Version::to_s.should == '0.2.1'
   end
 end
