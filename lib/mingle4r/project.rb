8ce6b13e464a454f2d975afbb67e4b9bce3a2fab
arusarh <arusarh@arusarhs-macbook-pro.local>
2009-07-21 17:00:10 +0530

    added property_definitions as a relationship method. also cards, users have an option to return either cache or non-cached results

--- lib/mingle4r/project.rb
+++ lib/mingle4r/project.rb
@@ -1,34 +1,53 @@
 module Mingle4r
   class Project
     module InstanceMethods
-      def cards
-        return @cards if @cards
+      def cards(refresh = false)
+        return @cards if(!refresh && @cards_cached)
         cards_site = File.join(self.class.site.to_s, "projects/#{self.identifier()}")
         Mingle4r::Card.site = cards_site
         Mingle4r::Card.user = self.class.user
         Mingle4r::Card.password = self.class.password
         card_class = Mingle4r::Card.send(:create_resource_class)
         @cards = card_class.find_without_pagination(:all)
+        @cards_cached = true
+        @cards
       end
       
-      def users
-        return @users if @users
+      def users(refresh = false)
+        return @users if(!refresh && @users_cached)
         users_site = File.join(self.class.site.to_s, "projects/#{self.identifier()}")
         Mingle4r::User.site = users_site
         Mingle4r::User.user = self.class.user
         Mingle4r::User.password = self.class.password
+        Mingle4r::User.element_name = nil # reset
         user_class = Mingle4r::User.send(:create_resource_class)
         @users = user_class.find(:all)
+        @users_cached = true
+        @users
       end
       
-      def wiki
-        return @wiki if @wiki
+      def wiki(refresh = false)
+        return @wiki if(!refresh && @wiki_cached)
         wiki_site = File.join(self.class.site.to_s, "projects/#{self.identifier()}")
         Mingle4r::Wiki.site = wiki_site
         Mingle4r::Wiki.user = self.class.user
         Mingle4r::Wiki.password = self.class.password
         wiki_class = Mingle4r::Wiki.send(:create_resource_class)
         @wiki = wiki_class.find(:all)
+        @wiki_cached = true
+        @wiki
+      end
+      
+      def property_definitions(refresh = false)
+        return @prop_definitions if(!refresh && @prop_definitions_cached)
+        properties_site = File.join(@site.to_s, "projects/#{self.identifier}")
+        PropertyDefinition.site = properties_site
+        PropertyDefinition.user = @user
+        PropertyDefinition.password = @password
+        prop_defn_class = Mingle4r::PropertyDefinition.send(:create_resource_class)
+        @prop_definitions = prop_defn_class.find(:all)
+        @prop_definitions_cached = true
+        @prop_definitions
       end
     end # module InstanceMethods
     
