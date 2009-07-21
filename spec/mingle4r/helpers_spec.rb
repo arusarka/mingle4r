0b53ef282c87006c5df7857243c1bff33b02ebca
arusarh <arusarh@arusarhs-macbook-pro.local>
2009-07-21 16:57:44 +0530

    added spec for encode2html method

--- spec/mingle4r/helpers_spec.rb
+++ spec/mingle4r/helpers_spec.rb
@@ -10,4 +10,11 @@ describe Mingle4r::Helpers do
     token2 = Mingle4r::Helpers::fast_token
     assert_not_equal(token1, token2)
   end
+  
+  it "should return a string in proper html format" do
+    string = 'Hello(whosoever) world []'
+    html_result = Mingle4r::Helpers::encode2html(string)
+    html_expected = 'Hello%28whosoever%29%20world%20%5B%5D'
+    html_result.should == html_expected
+  end
 end
