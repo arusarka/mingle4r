8133e4a3ace902e1f592ff6e2b3a37079a1e0403
asur <arusarka@gmail.com>
2010-06-04 11:52:27 +0530

    Removed version.rb

--- spec/mingle4r/version_spec.rb
+++ spec/mingle4r/version_spec.rb
@@ -2,13 +2,21 @@ require File.dirname(__FILE__) + '/../spec_helper'
 require 'rubygems/specification'
 
 describe "Mingle4r::Version" do
+  def version_in_VERSION_file
+    version_file_path = File.dirname(__FILE__) + '/../../VERSION'
+    IO.read(version_file_path).chomp
+  end
+  
+  def gemspec_version
+    gemspec_data = File.read(File.dirname(__FILE__) + '/../../mingle4r.gemspec')
+    eval(gemspec_data).version.to_s
+  end
+
   it "should return proper version" do
-    Mingle4r::Version::to_s.should == '0.4.5'
+    version_in_VERSION_file.should == '0.4.5'
   end
   
   it "should have the same version as in the gemspec" do
-    gemspec_data = File.read(File.dirname(__FILE__) + '/../../mingle4r.gemspec')
-    gemspec_version = eval(gemspec_data).version.to_s
-    Mingle4r::Version::to_s.should == gemspec_version
+    version_in_VERSION_file.should == gemspec_version
   end
 end
