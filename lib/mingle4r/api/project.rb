42f386e0f7cea2416d7d29c26702cf99684dcdb2
asur <arusarka@gmail.com>
2010-06-06 00:23:47 +0530

    project has a new card method and removed setting class set flag when setting attributes for a class

--- lib/mingle4r/api/project.rb
+++ lib/mingle4r/api/project.rb
@@ -4,44 +4,44 @@ module Mingle4r
       module InstanceMethods
         # returns the cards for the project.
         def cards
-          set_attributes_for(Card) unless card_class_set
+          set_attributes_for(Card)
           Card.find_without_pagination(:all)
         end
 
         # returns the users for the project.
         def users
-          set_attributes_for(User) unless user_class_set
+          set_attributes_for(User)
           User.find(:all)
         end
 
         # returns the wikis for the project.
         def wikis
-          set_attributes_for(Wiki) unless wiki_class_set
+          set_attributes_for(Wiki)
           Wiki.find(:all)
         end
 
         # returns the property definitions for the project.
         def property_definitions
-          set_attributes_for(PropertyDefinition) unless property_definition_class_set
+          set_attributes_for(PropertyDefinition)
           PropertyDefinition.find(:all)
         end
 
         # returns the murmurs for the project.
         def murmurs
-          set_attributes_for(Murmur) unless murmur_class_set
+          set_attributes_for(Murmur)
           Murmur.find(:all)
         end
 
         # posts a murmur
         def post_murmur(str)
-          set_attributes_for(Murmur) unless murmur_class_set
+          set_attributes_for(Murmur)
           murmur = Murmur.new(:body => str.to_s)
           murmur.save
         end
         
         # executes an mql
         def execute_mql(query)
-          set_attributes_for(ExecuteMql) unless execute_mql_class_set
+          set_attributes_for(ExecuteMql)
           ExecuteMql.query(query)
         end
         
@@ -57,6 +57,11 @@ module Mingle4r
           Card.apply_filter(filter_str)
         end
         
+        def new_card
+          set_attributes_for(Card)
+          Card.new
+        end
+        
         private
         def set_attributes_for(klass)
           resource_site = File.join(self.class.site.to_s, "projects/#{self.identifier}")
@@ -64,39 +69,7 @@ module Mingle4r
           klass.site = resource_site
           klass.user = self.class.user
           klass.password = self.class.password
-          setter_method = klass.name.demodulize.underscore + '_class_set'
-          send(setter_method, true)
           klass
-        end
-        
-        def user_class_set(val = nil)
-          return @user_class_set unless val
-          @user_class_set = val
-        end
-        
-        def card_class_set(val = nil)
-          return @card_class_set unless val
-          @card_class_set = val
-        end
-        
-        def wiki_class_set(val = nil)
-          return @wiki_class_set unless val
-          @wiki_class_set = val
-        end
-        
-        def property_definition_class_set(val = nil)
-          return @property_definition_class_set unless val
-          @property_definition_class_set = val
-        end
-        
-        def murmur_class_set(val = nil)
-          return @murmur_class_set unless val
-          @murmur_class_set = val
-        end
-        
-        def execute_mql_class_set(val = nil)
-          return @execute_mql_class_set unless val
-          @execute_mql_class_set = val
         end
       end # module InstanceMethods
 
--- spec/mingle4r/api/project_spec.rb
+++ spec/mingle4r/api/project_spec.rb
@@ -17,29 +17,7 @@ describe Project do
     @project = Project.new(:identifier => 'some-id')
   end
   
-  context "fetching users" do
-    it "should set the attributes for User class only once" do
-      User.stub!(:find)
-      User.should_receive(:site=).once
-      User.should_receive(:user=).once
-      User.should_receive(:password=).once
-      
-      @project.users
-      @project.users
-    end
-  end
-
   context "fetching cards" do
-    it "should set the attributes for Card class only once" do
-      Card.stub!(:find_without_pagination)
-      Card.should_receive(:site=).once
-      Card.should_receive(:user=).once
-      Card.should_receive(:password=).once
-      
-      @project.cards
-      @project.cards
-    end
-    
     it "should be able to ask for a single card" do
       Card.should_receive(:find).with(42)
       @project.find_card(42)
@@ -53,65 +31,24 @@ describe Project do
     end
   end
   
-  context "fetching wikis" do
-    it "should set the attributes for Wiki class only once" do
-      Wiki.stub!(:find)
-      Wiki.should_receive(:site=).once
-      Wiki.should_receive(:user=).once
-      Wiki.should_receive(:password=).once
+  context "when creating new card obejct" do
+    it "should set the card attributes" do
+      base_site = 'http://enterprise-server/mingle/api/v2/'
+      Project.site = base_site
+      Project.user = 'someone'
+      Project.password = 'very-secret'
+      project = Project.new(:identifier => 'foobar')
+      project.new_card
       
-      @project.wikis
-      @project.wikis
-    end
-  end
-  
-  context "fetching property definitions" do
-    it "should set the attributes for PropertyDefinition class only once" do
-      PropertyDefinition.stub!(:find)
-      PropertyDefinition.should_receive(:site=).once
-      PropertyDefinition.should_receive(:user=).once
-      PropertyDefinition.should_receive(:password=).once
-      
-      @project.property_definitions
-      @project.property_definitions
-    end
-  end
-  
-  context "murmurs" do
-    def any_murmur
-      OpenStruct.new(:save => 'do nothing')
+      Card.site.should == File.join(base_site, 'projects', project.identifier)
+      Card.user.should == 'someone'
+      Card.password.should == 'very-secret'
     end
     
-    it "should set the attributes for Murmur class only once when fetching" do
-      Murmur.stub!(:find)
-      Murmur.should_receive(:site=).once
-      Murmur.should_receive(:user=).once
-      Murmur.should_receive(:password=).once
-      
-      @project.murmurs
-      @project.murmurs
-    end
-    
-    it "should set the attributes for Murmur class only once when posting" do
-      Murmur.stub!(:new).and_return(any_murmur)
-      Murmur.should_receive(:site=).once
-      Murmur.should_receive(:user=).once
-      Murmur.should_receive(:password=).once
-      
-      @project.post_murmur('hello')
-      @project.post_murmur('world')
-    end
-  end
-  
-  context "executing an mql" do
-    it "should set the attributes of the ExecuteMql class only once" do
-      ExecuteMql.stub!(:query)
-      ExecuteMql.should_receive(:site=).once
-      ExecuteMql.should_receive(:user=).once
-      ExecuteMql.should_receive(:password=).once
-      
-      @project.execute_mql('Select name WHERE type is Story')
-      @project.execute_mql('Select name WHERE type is Defect')
+    it "should instantiate a card object" do
+      card_obj = mock('Card')
+      Card.should_receive(:new).and_return(card_obj)
+      @project.new_card.should == card_obj
     end
   end
   
